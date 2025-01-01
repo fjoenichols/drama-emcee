ab7f3b4f416d7807ff507235b5a81898ad922f4d
Joe Nichols <joe@fjoenichols.com>
2024-12-31 19:03:09 -0700

    fix crafter name redis path

--- utility.py
+++ utility.py
@@ -8,6 +8,7 @@ def who_knows_recipe( recipe ):
     try:
         crafters = r.get( "professions: recipes: " + recipe )
         c_list = crafters_list(crafters)
+
         return "The following players know **" + recipe.title() + "**\n\n" + c_list
     except: 
         return "Nobody in the guild knows **" + recipe.title() + "**."
@@ -20,8 +21,8 @@ def crafters_list( crafters ):
         for crafter in crafters_array:
             player_name = crafter.split("+")[0]
             player_realm = crafter.split("+")[1]
-            proper_name = json.loads( redis_conf.r.get("player_professions: " + player_realm + ": " + player_name) ).get("character").get("name")
-            proper_realm = json.loads( redis_conf.r.get("player_professions: " + player_realm + ": " + player_name) ).get("character").get("realm").get("name")
+            proper_name = json.loads( redis_conf.r.get("player_professions: " + player_name + "+" + player_realm) ).get("character").get("name")
+            proper_realm = json.loads( redis_conf.r.get("player_professions: " + player_name + "+" + player_realm) ).get("character").get("realm").get("name")
 
             c.append(proper_name + "-" + proper_realm)
 
