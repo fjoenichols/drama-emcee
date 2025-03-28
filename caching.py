33f546e45ca457d308d6e99837ab80e36bbf72fb
Joe Nichols <joe@fjoenichols.com>
2025-03-28 00:12:23 -0600

    add error checking to caching

--- caching.py
+++ caching.py
@@ -19,16 +19,22 @@ def guild_roster_update( region, locale, guild_slug, guild_realm_slug ):
 
 def character_professions_update( character_realm_slug, character_name_lower ):
     """Retrieves character profession information as json output from battle.net's world of warcraft profile api and caches it to redis"""
-    character_professions = api_client.wow.profile.get_character_professions_summary( 'us', 'en_us', character_realm_slug, character_name_lower )
-    r.set( "player_professions: " + character_name_lower + "+" + character_realm_slug, json.dumps( character_professions ) )
+    try:
+        character_professions = api_client.wow.profile.get_character_professions_summary( 'us', 'en_us', character_realm_slug, character_name_lower )
+        r.set( "player_professions: " + character_name_lower + "+" + character_realm_slug, json.dumps( character_professions ) )
+    except:
+        print( "Error updating " + character_name_lower + "+" + character_realm_slug ) 
     return 
 
 def guild_roster_character_professions_update( guild_roster ):
     """Loops through an entire guild roster calling character_professions_update()"""
     for member in guild_roster:
-        character_name = member.get( "character" ).get( "name" ).lower()
-        character_realm_slug = member.get( "character" ).get( "realm" ).get( "slug" )
-        print( "updating " + character_name.lower() + "+" + character_realm_slug )
-        character_professions_update( character_realm_slug, character_name )
+        try:
+            character_name = member.get( "character" ).get( "name" ).lower()
+            character_realm_slug = member.get( "character" ).get( "realm" ).get( "slug" )
+            print( "updating " + character_name.lower() + "+" + character_realm_slug )
+            character_professions_update( character_realm_slug, character_name )
+        except:
+            print( "Error updating " + character_name.lower() + "+" + character_realm_slug ) 
     return
 
